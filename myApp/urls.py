650d9393ba49e6c2f271451091676b6b54e609d9
salsaden <abaaysalmo@gmail.com>
2023-08-03 11:00:05 +0300

    initial commit

---
+++ myApp/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from myApp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.register,name='register'),
+    path('login/', views.login, name='login'),
+    path('home/', views.home, name='home'),
+]
